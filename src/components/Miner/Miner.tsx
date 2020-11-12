7c47a09b5c4741469534d551c68ef7e7d7b7235b
serominer <serominer@gmail.com>
2020-11-12 21:57:30 +0800

    update

--- src/components/Miner/Miner.tsx
+++ src/components/Miner/Miner.tsx
@@ -173,14 +173,16 @@ class Miner extends React.Component<any, Miners> {
     const that = this;
     contract.details(mainpkr).then((res) => {
       that.level(fromValue(res[2].amount, 18).toNumber());
-      let recommendProfit = 0;
-      let nodeProfit = 0;
-      let communityProfit = 0;
-      if (Math.floor(res[2].dynamicTimestamp / 600) == Math.floor(new Date().getTime() / 1000 / 600)) {
-        recommendProfit = parseFloat(fromValue(res[2].recommendProfit, 18).toNumber().toFixed(2));
-        nodeProfit = parseFloat(fromValue(res[2].nodeProfit, 18).toNumber().toFixed(2));
-        communityProfit = parseFloat(fromValue(res[2].communityProfit, 18).toNumber().toFixed(2));
-      }
+      // let recommendProfit = 0;
+      // let nodeProfit = 0;
+      // let communityProfit = 0;
+      // if (Math.floor(res[2].dynamicTimestamp / 600) == Math.floor(new Date().getTime() / 1000 / 600)) {
+      //   recommendProfit = parseFloat(fromValue(res[2].recommendProfit, 18).toNumber().toFixed(2));
+      //   nodeProfit = parseFloat(fromValue(res[2].nodeProfit, 18).toNumber().toFixed(2));
+      //   communityProfit = parseFloat(fromValue(res[2].communityProfit, 18).toNumber().toFixed(2));
+      // }
+    
+
       that.setState({
         achievement: parseFloat(fromValue(res[2].achievement, 18).toNumber().toFixed(2)),
         myid: res[0],
@@ -193,9 +195,14 @@ class Miner extends React.Component<any, Miners> {
         returnnowday: parseFloat(fromValue(res[2].amount, 18).multipliedBy(0.003).toNumber().toFixed(2)),
         amount: parseFloat(fromValue(res[2].amount, 18).toNumber().toFixed(2)),
         lastreturntime: that.formatTime(res[2][9] * 1000, 'M/D h:m'),
-        recommendProfit: recommendProfit,
-        nodeProfit: nodeProfit,
-        communityProfit: communityProfit
+
+        recommendProfit: parseFloat(fromValue(res[2].recommendProfit, 18).toNumber().toFixed(2)),
+        nodeProfit: parseFloat(fromValue(res[2].nodeProfit, 18).toNumber().toFixed(2)),
+        communityProfit: parseFloat(fromValue(res[2].communityProfit, 18).toNumber().toFixed(2))
+
+        // recommendProfit: recommendProfit,
+        // nodeProfit: nodeProfit,
+        // communityProfit: communityProfit
       })
     })
   }
